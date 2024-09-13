5d6171681e032896a351fbf5fbf537eda57f9a72
Hello World! <141351256+Pankaj903126@users.noreply.github.com>
2024-09-13 19:08:05 +0530

    Update login.js

--- login.js
+++ login.js
@@ -10,7 +10,7 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
                 errorMessage.textContent = '';
                 errorMessage.style.opacity = 0;
                 // Redirect to a new page (replace 'YOUR_REDIRECT_URL' with the actual URL)
-                window.location.href = 'Google Clone.html';
+                window.location.href = 'https://www.google.com';
             } else {
                 errorMessage.textContent = 'Invalid username or password. Please try again.';
                 errorMessage.style.opacity = 1;
